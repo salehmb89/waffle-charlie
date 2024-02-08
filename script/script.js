beee83af3bd4b80a2a59a368fa33c182036419b1
Saleh Basalim <156353463+salehmb89@users.noreply.github.com>
2024-02-07 18:03:38 -0600

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,23 +1,25 @@
-$(document).ready(function() {
-  
-  console.log("Top: " + $("img").position().top + " Left: " + $("img").position().left);
- 
- $("img").draggable({
-   stop: function() {
-     console.log("stopping????");
-   
-     let dogeLeft = $("img").position().left;
+$(document).ready(function () {
+  console.log("Top: " + $("#dogePic").position().top + " Left: " + $("#dogePic").position().left);
 
-     if (dogeLeft > 500) {
+  $("#dogePic").draggable({
+    stop: function () {
+      console.log("Stopping???");
 
-       console.log("we above 500");
-       $("p").text("you win.. good for you");
+      // Check if the image is on the far-right side
+      let dogeLeft = $("#dogePic").position().left;
 
+      if (dogeLeft > 300) {
+        console.log("We are above 300", dogeLeft);
 
-      //stop: function() {
-       //alert( $("img").position().left );
-      //alert("Top: " + $("img").position().top + " Left: " +$("img").position().left);
-     }
-   }
- });
+        $("p").text("You won. Good for you.");
+
+        // Change image source on winning
+        $("#dogePic").attr("src", "https://picsum.photos/200/300");
+      } else {
+        console.log("We are NOT above 300");
+      }
+    },
+    containment: "#containment-wrapper",
+    scroll: false
+  });
 });
